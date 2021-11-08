aab6f51dbea714111f0f4e01c0168d0fa7b5ffed
rmunoz <richosojason@msn.com>
2021-11-08 17:03:26 -0500

    added nested routes/components

---
+++ next-project/pages/about.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const About: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>The about page</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ next-project/pages/portfolio/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import styles from "../../styles/Home.module.css";
+
+const Portfolio: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>The Portfolio Page</h1>
+    </div>
+  );
+};
+
+export default Portfolio;
