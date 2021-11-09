c4ac553d63c0b498d7d8f510f34693606ea3d198
rmunoz <richosojason@msn.com>
2021-11-08 23:30:30 -0500

    added not found page

---
+++ next-project/pages/404.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const NotFound: NextPage = () => {
+  return (
+    <div>
+      <h1>Page not found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
--- next-project/pages/clients/[clientId]/index.tsx
+++ next-project/pages/clients/[clientId]/index.tsx
@@ -1,9 +1,17 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const ClientProjects: NextPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    router.push("/clients/richard/project-a");
+  };
+
   return (
     <div>
       <h1>The Projects of a given Client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 };
