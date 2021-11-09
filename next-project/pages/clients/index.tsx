30e244f451e1c2fcf87f4fec8625fa1d2c86e678
rmunoz <richosojason@msn.com>
2021-11-08 23:21:19 -0500

    added links to clients page

--- next-project/pages/clients/index.tsx
+++ next-project/pages/clients/index.tsx
@@ -1,9 +1,25 @@
 import { NextPage } from "next";
+import Link from "next/link";
 
 const Clients: NextPage = () => {
+  const clients = [
+    { id: "richard", name: "Richard" },
+    {
+      id: "john",
+      name: "John",
+    },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
