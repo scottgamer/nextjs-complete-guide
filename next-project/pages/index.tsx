44ccf4599362d74bb5803301dad42d8ed5ba457d
rmunoz <richosojason@msn.com>
2021-11-08 23:21:10 -0500

    added links to home page

--- next-project/pages/index.tsx
+++ next-project/pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1>Hello Next World!</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
