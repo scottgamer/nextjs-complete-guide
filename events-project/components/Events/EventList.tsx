62c17e8bb8619e9f00a8ce5c9fb06079ce46395f
rmunoz <richosojason@msn.com>
2021-11-14 19:37:14 -0500

    added event list styles to component

--- events-project/components/Events/EventList.tsx
+++ events-project/components/Events/EventList.tsx
@@ -1,10 +1,10 @@
 import { Event } from "../../interfaces/events";
 import EventItem from "./EventItem";
+import styles from "./EventList.module.css";
 
 const EventList = ({ events }: { events: Event[] }): JSX.Element => {
-  console.log(events);
   return (
-    <ul>
+    <ul className={styles.list}>
       {events.map((event) => (
         <EventItem key={event.id} event={event} />
       ))}
