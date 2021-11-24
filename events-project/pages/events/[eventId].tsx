4018c08c27a03f8a55f6d9ff23ad275a4391b1b9
rmunoz <richosojason@msn.com>
2021-11-23 23:49:07 -0500

    added event details page component

--- events-project/pages/events/[eventId].tsx
+++ events-project/pages/events/[eventId].tsx
@@ -1,11 +1,29 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import EventContent from "../../components/EventDetail/eventContent";
+import EventLogistics from "../../components/EventDetail/eventLogistics";
+import EventSummary from "../../components/EventDetail/eventSummary";
+import { getEventById } from "../../data/dummy-data";
 
-const Event: NextPage = () => {
+const EventDetail: NextPage = () => {
+  const router = useRouter();
+
+  const eventId: string | string[] | undefined = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>The Event Detail Page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics event={event} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 };
 
-export default Event;
+export default EventDetail;
