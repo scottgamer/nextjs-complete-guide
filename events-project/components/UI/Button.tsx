1945b46dfa667267465cf960648ba5cd0ee4bd94
rmunoz <richosojason@msn.com>
2022-06-14 16:30:00 -0500

    added button conditional

--- events-project/components/UI/Button.tsx
+++ events-project/components/UI/Button.tsx
@@ -5,14 +5,20 @@ import styles from "./Button.module.css";
 const Button = ({
   children,
   link,
+  onClick,
 }: {
   children: React.ReactNode;
-  link: string;
+  link?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }): JSX.Element => {
-  return (
+  return link ? (
     <Link href={link}>
       <a className={styles.btn}>{children}</a>
     </Link>
+  ) : (
+    <button className={styles.btn} onClick={onClick}>
+      {children}
+    </button>
   );
 };
 
