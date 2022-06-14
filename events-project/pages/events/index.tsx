8ec330a12415f2a0867d53271b5585d822c5501d
rmunoz <richosojason@msn.com>
2022-06-14 16:30:29 -0500

    added filter to events index

--- events-project/pages/events/index.tsx
+++ events-project/pages/events/index.tsx
@@ -1,9 +1,25 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import EventList from "../../components/Events/EventList";
+import EventsSearch from "../../components/Events/EventsSearch";
+import { getAllEvents } from "../../data/dummy-data";
 
 const Events: NextPage = () => {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (
+    year: string | undefined,
+    month: string | undefined
+  ) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
   return (
     <div>
-      <h1>The Events page</h1>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList events={events} />
     </div>
   );
 };
