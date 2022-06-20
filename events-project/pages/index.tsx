1152dba666933ec18526135441a4902c2a1d80ea
rmunoz <richosojason@msn.com>
2022-06-20 16:24:37 -0500

    added get static props to home

--- events-project/pages/index.tsx
+++ events-project/pages/index.tsx
@@ -1,18 +1,31 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import EventList from "../components/Events/EventList";
-import { getFeaturedEvents } from "../data/dummy-data";
+import { getFeaturedEvents } from "../helpers/apiUtils";
+import { Event } from "../interfaces/events";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
-  const featuredEvents = getFeaturedEvents();
+interface HomeProps {
+  events: Event[];
+}
 
+const Home: NextPage<HomeProps> = ({ events }) => {
   return (
     <div className={styles.container}>
       <ul>
-        <EventList events={featuredEvents} />
+        <EventList events={events} />
       </ul>
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
+};
