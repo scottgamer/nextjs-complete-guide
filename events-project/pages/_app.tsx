71c06e7c862c2fdfa2cf5e76c37d3f58de57e5ef
rmunoz <richosojason@msn.com>
2022-06-22 14:57:06 -0500

    added global header

--- events-project/pages/_app.tsx
+++ events-project/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      {/* global head metadata */}
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Find a lot of great events here" />
+        <title>NextJS Events</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
