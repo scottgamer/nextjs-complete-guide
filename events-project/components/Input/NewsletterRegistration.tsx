7ffbb4d5a668079f103d68d30719b0f629504fbb
rmunoz <richosojason@msn.com>
2022-06-24 16:10:47 -0500

    added form submission

--- events-project/components/Input/NewsletterRegistration.tsx
+++ events-project/components/Input/NewsletterRegistration.tsx
@@ -1,15 +1,32 @@
 import { NextPage } from "next";
-import classes from "./newsletter-registration.module.css";
+import React, { useRef } from "react";
+import classes from "./NewsletterRegistration.module.css";
 
 //FIX: event handler typing
 const NewsletterRegistration: NextPage = () => {
-  function registrationHandler(event) {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+
+  const registrationHandler = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
     event.preventDefault();
 
     // fetch user input (state or refs)
     // optional: validate input
     // send valid data to API
-  }
+
+    const enteredEmail = emailInputRef.current?.value;
+
+    const response = await fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <section className={classes.newsletter}>
@@ -21,6 +38,7 @@ const NewsletterRegistration: NextPage = () => {
             id="email"
             placeholder="Your email"
             aria-label="Your email"
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
