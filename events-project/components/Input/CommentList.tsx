e4bca3469027b298f6d1b6e79b7ee0fdf03f352e
rmunoz <richosojason@msn.com>
2022-06-24 11:41:23 -0500

    added comments and newsletter components

---
+++ events-project/components/Input/CommentList.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+import classes from "./CommentList.module.css";
+
+const CommentList: NextPage = () => {
+  return (
+    <ul className={classes.comments}>
+      {/* Render list of comments - fetched from API */}
+      <li>
+        <p>My comment is amazing!</p>
+        <div>
+          By <address>Maximilian</address>
+        </div>
+      </li>
+      <li>
+        <p>My comment is amazing!</p>
+        <div>
+          By <address>Maximilian</address>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default CommentList;
---
+++ events-project/components/Input/Comments.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import CommentList from "./CommentList";
+import NewComment from "./NewComment";
+import classes from "./comments.module.css";
+import { NextPage } from "next";
+
+
+// FIX: add props
+const Comments: NextPage = ({ eventId }) => {
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList />}
+    </section>
+  );
+};
+
+export default Comments;
