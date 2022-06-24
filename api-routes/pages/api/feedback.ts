271b35f96d1aca62ff62a9f310a2aa63fd8a1353
rmunoz <richosojason@msn.com>
2022-06-23 22:43:27 -0500

    added feedback api route

--- api-routes/pages/api/feedback.ts
+++ api-routes/pages/api/feedback.ts
@@ -1,10 +1,37 @@
 import { NextApiHandler } from "next";
+import fs from "fs/promises";
+import path from "path";
 
 // allows to execute server-side code
-const handler: NextApiHandler = (req, res) => {
-  res.status(200).json({
-    message: "this works",
-  });
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const text = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text,
+    };
+
+    try {
+      // store in DB or file
+      const filePath = path.join(process.cwd(), "data", "feedback.json");
+      const fileData = await fs.readFile(filePath);
+      const data = JSON.parse(fileData.toString());
+      const updatedData = [...data, newFeedback];
+
+      await fs.writeFile(filePath, JSON.stringify(updatedData));
+    } catch (error) {
+      console.error(error);
+    }
+
+    res.status(201).json({ message: "Success", feedback: newFeedback });
+  } else {
+    res.status(200).json({
+      message: "this works",
+    });
+  }
 };
 
 export default handler;
