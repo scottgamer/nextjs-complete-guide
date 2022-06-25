6a9a9bcf814fc3094a635a897ae7e6dd89128ed2
rmunoz <richosojason@msn.com>
2022-06-24 22:14:57 -0500

    added fetch comments function

--- events-project/components/Input/Comments.tsx
+++ events-project/components/Input/Comments.tsx
@@ -1,24 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import CommentList from "./CommentList";
 import NewComment from "./NewComment";
-import classes from "./comments.module.css";
+import classes from "./Comments.module.css";
 import { NextPage } from "next";
 
 interface CommentsProps {
   eventId: string;
 }
 
+export interface Comment {
+  id: string;
+  email: string;
+  name: string;
+  text: string;
+}
+
 // FIX: add props
 const Comments: NextPage<CommentsProps> = ({ eventId }) => {
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const getComments = async () => {
+      if (showComments) {
+        const response = await fetch(`/api/comments/${eventId}`);
+        const data = await response.json();
+        setComments(data.comments);
+      }
+    };
+
+    getComments();
+  }, [showComments, eventId]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
-  function addCommentHandler(commentData) {
+  async function addCommentHandler(comment: Comment) {
     // send data to API
+    const response = await fetch(`/api/comments/${eventId}`, {
+      method: "POST",
+      body: JSON.stringify(comment),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
@@ -27,7 +57,7 @@ const Comments: NextPage<CommentsProps> = ({ eventId }) => {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList comments={comments} />}
     </section>
   );
 };
