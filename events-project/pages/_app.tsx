53b7693354bb7b2cb3dd6d200b9c9c53f8c845b7
rmunoz <richosojason@msn.com>
2022-06-29 22:27:39 -0500

    added notification context to app

--- events-project/pages/_app.tsx
+++ events-project/pages/_app.tsx
@@ -2,18 +2,30 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
 import Head from "next/head";
+import Notification from "../components/UI/Notification";
+import { NotificationProvider } from "../store/notificationContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      {/* global head metadata */}
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Find a lot of great events here" />
-        <title>NextJS Events</title>
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationProvider>
+      <Layout>
+        {/* global head metadata */}
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta name="description" content="Find a lot of great events here" />
+          <title>NextJS Events</title>
+        </Head>
+        <Component {...pageProps} />
+        <Notification
+          title={"test"}
+          message={"This is a test"}
+          status={"pending"}
+        />
+      </Layout>
+    </NotificationProvider>
   );
 }
 
