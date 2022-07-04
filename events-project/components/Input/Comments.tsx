421cb38a11d6c869ec164576c20c5ec7a0bc3390
rmunoz <richosojason@msn.com>
2022-07-03 22:57:23 -0500

    added loading state to comments component

--- events-project/components/Input/Comments.tsx
+++ events-project/components/Input/Comments.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CommentList from "./CommentList";
 import NewComment from "./NewComment";
 import classes from "./Comments.module.css";
 import { NextPage } from "next";
 import { Document } from "mongodb";
+import NotificationContext from "../../store/notificationContext";
 interface CommentsProps {
   eventId: string;
 }
@@ -19,13 +20,17 @@ export interface Comment extends Document {
 const Comments: NextPage<CommentsProps> = ({ eventId }) => {
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const { showNotification } = useContext(NotificationContext);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   useEffect(() => {
     const getComments = async () => {
       if (showComments) {
+        setIsFetchingComments(true);
         const response = await fetch(`/api/comments/${eventId}`);
         const data = await response.json();
         setComments(data.comments);
+        setIsFetchingComments(false);
       }
     };
 
@@ -37,17 +42,38 @@ const Comments: NextPage<CommentsProps> = ({ eventId }) => {
   }
 
   async function addCommentHandler(comment: Comment) {
-    // send data to API
-    const response = await fetch(`/api/comments/${eventId}`, {
-      method: "POST",
-      body: JSON.stringify(comment),
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
+    try {
+      showNotification({
+        title: "Sending comment",
+        message: "Your comment is currently being stored into a DB",
+        status: "pending",
+      });
+      // send data to API
+      const response = await fetch(`/api/comments/${eventId}`, {
+        method: "POST",
+        body: JSON.stringify(comment),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      if (response.ok) {
+        showNotification({
+          title: "Success!",
+          message: "Your comment was saved!",
+          status: "success",
+        });
+        return await response.json();
+      }
 
-    const data = await response.json();
-    console.log(data);
+      const data = await response.json();
+      throw new Error(data.message) || "Something went wrong";
+    } catch (error) {
+      showNotification({
+        title: "Error",
+        message: (error as Error).message ?? "Error registering to newsletter",
+        status: "error",
+      });
+    }
   }
 
   return (
@@ -56,7 +82,19 @@ const Comments: NextPage<CommentsProps> = ({ eventId }) => {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList comments={comments} />}
+      {showComments && !isFetchingComments && (
+        <CommentList comments={comments} />
+      )}
+      {showComments && isFetchingComments && <p>Loading...</p>}
+
+      {/* {showComments && isFetchingComments ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <NewComment onAddComment={addCommentHandler} />
+          <CommentList comments={comments} />
+        </>
+      )} */}
     </section>
   );
 };
